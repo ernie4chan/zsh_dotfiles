d3821dbb2d1f8f0c37e99440d33f173cd3fdf039
Ernie Lin <ernie.lin@live.com>
2018-10-21 12:31:25 -0300

    20181020 updates

--- modules/private_tokens/init.zsh
+++
@@ -1,7 +0,0 @@
-# macOS Homebrew
-
-# Github API token for Homebrew.
-export HOMEBREW_GITHUB_API_TOKEN=c2f5a5d8a2437becdb7b8f8a902101b760d777c1
-
-# Prevent Homebrew from gathering analytics.
-export HOMEBREW_NO_ANALYTICS=1
