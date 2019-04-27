65638dadec117a62c847f137f0120c1e06c98a98
Ernie Lin <ernie.lin@live.com>
2019-04-27 15:46:18 -0300

    20190426 updates

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -3,14 +3,22 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="$PATH:/usr/local/opt/fzf/bin"
 fi
 
-# Use 'ripgrep' by default.
-export FZF_DEFAULT_COMMAND="rg --color=never --files --hidden -g '*' -g '!.git/'"
+# Use ~~ as the trigger sequence instead of the default **.
+#export FZF_COMPLETION_TRIGGER='~~'
 
-# Changing the layout.
-export FZF_DEFAULT_OPTS="--height=40% --preview='cat {}' --preview-window=right:60%:wrap"
+# Options to fzf command.
+#export FZF_COMPLETION_OPTS='+c -x'
+
+# Use 'ripgrep' by default.
+export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD ||
+	find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
+	sed s/^..//) 2> /dev/null'
 
 # Configure fzf in command line.
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+
+# Changing the layout.
+export FZF_DEFAULT_OPTS="--height=40% --preview='cat {}' --preview-window=right:60%:wrap"
 
 # Auto-completion
 # ---------------
