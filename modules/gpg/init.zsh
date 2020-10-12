42b8539f056a61a0a748314421665fce76536dd1
Ernie Lin <ernie.lin@live.com>
2020-10-12 01:41:27 -0300

    20201011 big updates

--- modules/gpg/init.zsh
+++ modules/gpg/init.zsh
@@ -6,20 +6,21 @@
 
 # Return if requirements are not found.
 if (( ! $+commands[gpg-agent] )); then
-  return 1
+	return 1
 fi
 
 # Set the default paths to gpg-agent files.
 _gpg_agent_conf="${GNUPGHOME:-$HOME/.gnupg}/gpg-agent.conf"
-_gpg_agent_env="${TMPDIR:-/tmp}/gpg-agent.env.$UID"
+_gpg_agent_env="${XDG_CACHE_HOME:-$HOME/.cache}/gpg-agent.env"
 
-# Load environment variables from previous run
+# Load environment variables from previous run.
 source "$_gpg_agent_env" 2> /dev/null
 
 # Start gpg-agent if not started.
-if [[ -z "$GPG_AGENT_INFO" && ! -S "${GNUPGHOME:-$HOME/.gnupg}/S.gpg-agent" ]]; then
-  # Start gpg-agent if not started.
+if [[ -z "$GPG_AGENT_INFO" && ! -S "$(gpgconf --list-dir agent-ssh-socket)" ]]; then
+	# Start gpg-agent if not started.
 	if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${${${(s.:.)GPG_AGENT_INFO}[2]}:--1} gpg-agent"; then
+		mkdir -p "$_gpg_agent_env:h"
 		eval "$(gpg-agent --daemon | tee "$_gpg_agent_env")"
 	fi
 fi
@@ -29,20 +30,20 @@ export GPG_TTY="$(tty)"
 
 # Integrate with the SSH module.
 if grep '^enable-ssh-support' "$_gpg_agent_conf" &> /dev/null; then
-  # Load required functions.
-  autoload -Uz add-zsh-hook
+	# Load required functions.
+	autoload -Uz add-zsh-hook
 
-  # Override the ssh-agent environment file default path.
-  _ssh_agent_env="$_gpg_agent_env"
+	# Override the ssh-agent environment file default path.
+	_ssh_agent_env="$_gpg_agent_env"
 
-  # Load the SSH module for additional processing.
-  pmodload 'ssh'
+	# Load the SSH module for additional processing.
+	pmodload 'ssh'
 
-  # Updates the GPG-Agent TTY before every command since SSH does not set it.
-  function _gpg-agent-update-tty {
-    gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null
-  }
-  add-zsh-hook preexec _gpg-agent-update-tty
+	# Updates the GPG-Agent TTY before every command since SSH does not set it.
+	function _gpg-agent-update-tty {
+		gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null
+	}
+	add-zsh-hook preexec _gpg-agent-update-tty
 fi
 
 # Clean up.
@@ -50,5 +51,5 @@ unset _gpg_agent_{conf,env}
 
 # Disable GUI prompts inside SSH.
 if [[ -n "$SSH_CONNECTION" ]]; then
-  export PINENTRY_USER_DATA='USE_CURSES=1'
+	export PINENTRY_USER_DATA='USE_CURSES=1'
 fi
--- zshrc
+++ zshrc
@@ -1,12 +1,5 @@
 # vim: ft=zsh
 
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zsh/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-#if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#	source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-#fi
-
 # Failsafe test purposes and debugging.
 #zsh -x 2> ${HOME}/zsh-error.log
 
@@ -27,6 +20,7 @@ zstyle ':zmodule:load' pmodule \
 	'gnu_utility' \
 	'aliases' \
 	'fzf' \
+	'gpg' \
 	'tmux' \
 	'autosuggestions' \
 	'completions' \
