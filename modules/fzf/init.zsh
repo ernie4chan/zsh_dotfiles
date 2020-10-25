592de39579ac12d964503096656755a0c913b1db
Ernie Lin <ernie.lin@live.com>
2020-10-25 14:19:26 -0300

    20201025 updates

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -12,17 +12,15 @@ pmodload 'helper'
 
 # Add fzf path.
 if is-linux; then
-	if [[ ! "$PATH" == */usr/share/fzf* ]]; then
+	[[ ! "$PATH" == */usr/share/fzf* ]] && \
 		export PATH="$PATH:/usr/share/fzf"
-	fi
 	# Auto-completion and Key-bindings.
 	[[ $- == *i* ]] && \
 		source "/usr/share/fzf/completion.zsh" 2>/dev/null; \
 		source "/usr/share/fzf/key-bindings.zsh" 2>/dev/null
 elif is-darwin; then
-	if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+	[[ ! "$PATH" == */usr/local/opt/fzf/bin* ]] && \
 		export PATH="$PATH:/usr/local/opt/fzf/bin"
-	fi
 	# Auto-completion and Key-bindings.
 	[[ $- == *i* ]] && \
 		source "/usr/local/opt/fzf/bin/completion.zsh" 2> /dev/null; \
@@ -30,10 +28,4 @@ elif is-darwin; then
 fi
 
 # Use 'ripgrep' by default by pressing <Ctrl-R>.
-if (( $+commands[rg] )); then
-	export FZF_DEFAULT_COMMAND='rg --files --follow --hidden --no-ignore-vcs'
-else
-	export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD ||
-		find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
-		sed s/^..//) 2> /dev/null'
-fi
+(( $+commands[rg] )) && export FZF_DEFAULT_COMMAND='rg --files --follow --hidden --no-ignore-vcs'
