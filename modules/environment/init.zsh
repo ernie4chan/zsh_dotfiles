5f7e2f1b7e608019215e93e2491301b4082ebc19
Ernie Lin <ernie4chan@users.noreply.github.com>
2022-06-15 12:01:41 -0300

    20220615 updates

--- modules/environment/init.zsh
+++ modules/environment/init.zsh
@@ -35,15 +35,10 @@ fi
 # {{{ --- Shell Behavior. ---
 
 # Behaviour.
-if ! is-darwin; then
-# Combine zero-length punctuation characters (accents) with the base character.
-	setopt COMBINING_CHARS
-# Beep on error.
-	setopt BEEP
-fi
-
+setopt COMBINING_CHARS	# Combine zero-length punctuation characters (accents) with the base character.
 setopt INTERACTIVE_COMMENTS	# Enable comments in interactive shell.
 setopt RC_QUOTES		# Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
+unsetopt BEEP			# Beep on error.
 unsetopt MAIL_WARNING	# Don't print a warning message if a mail file has been accessed.
 
 # Jobs.
