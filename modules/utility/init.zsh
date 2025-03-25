f67bab60d8723b265b95fb09a48790ff391a15e5
Ernie Lin <ernie4chan@users.noreply.github.com>
2025-03-25 11:24:39 +0800

    20250325 updates

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -125,15 +125,15 @@ else
 	fi
 fi
 
-alias l='ls -1'	# Lists in one column, hidden files.
+alias l='ls -F1'	# Lists in one column, hidden files.
 alias ll='ls -lh'	# Lists human readable sizes.
-alias lr='ll -R'	# Lists human readable sizes, recursively.
-alias la='ll -A'	# Lists human readable sizes, hidden files.
+alias la='ls -lhA'	# Lists human readable sizes, hidden files.
+alias lr='ls -lhR'	# Lists human readable sizes, recursively.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
 alias lk='ll -Sr'	# Lists sorted by size, largest last.
 alias lt='ll -tr'	# Lists sorted by date, most recent last.
-alias lc='lt -c'	# Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'	# Lists sorted by date, most recent last, shows access time.
+alias lc='ll -trc'	# Lists sorted by date, most recent last, shows change time.
+alias lu='ll -tru'	# Lists sorted by date, most recent last, shows access time.
 
 # Lists sorted by extension (GNU only).
 if [[ ${(@M)${(f)"$(ls --version 2>&1)"}:#*GNU *} ]]; then
@@ -169,7 +169,9 @@ elif is-termux; then
 	alias pbcopy='termux-clipboard-set'
 	alias pbpaste='termux-clipboard-get'
 else
-	alias oo='xdg-open'
+	if (( $+commands[xdg-open] )); then
+		alias oo='xdg-open'
+	fi
 
 	if (( $+commands[xclip] )); then
 		alias pbcopy='xclip -selection clipboard -in'
